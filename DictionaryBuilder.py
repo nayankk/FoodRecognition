8d9b62c2ff48f6a7318e1f5b7509c27ca1a66d3e
Nayan Kumar K <qtc746@motorola.com>
2014-03-09 21:05:20 -0700

    First version of DictionaryBuilder

---
+++ DictionaryBuilder.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+# Function used to build dictionary of code words
+
+# Algorithm
+# 1. For given subset of images, get the SURF descriptors for each of the image
+# 2. Plot these descriptors in high dimensional space
+# 3. Run k-means to computer 50 clusters and return these clusters as dictionary
+
+# TODO:
+# Have better logic of computing the dictionary
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import gc
+
+def plotHistogram(data):
+    plt.hist(data,50)
+    plt.show()
+
+def plotKeypoints(imgPath, kp):
+    img = cv2.imread(imgPath)
+    imgShow = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
+    plt.imshow(imgShow)
+    plt.show()
+
+def findSurfDescriptor(filename):
+    img = cv2.imread(filename)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    surf = cv2.SURF(hessianThreshold=100, extended=False)
+    kp, des = surf.detectAndCompute(imgGray, None)
+    return des
+
+def buildDictionary(rootPath):
+    surfDes = np.empty([1,64])
+    for path, subdirs, files in os.walk(rootPath):
+        for filename in files:
+            if filename.endswith("_thumb.jpg"):
+                filename = os.path.join(path, filename)
+                surfDesRow = findSurfDescriptor(filename)
+                surfDes = np.vstack((surfDes, surfDesRow))
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret,label,center=cv2.kmeans(np.float32(surfDes), 50, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    del surfDes
+    gc.collect()
+    return center
+
+def main():
+    root = "/Users/qtc746/Documents/Courses/ComputerVision/Project/Dataset/Dictionary/"
+    center = buildDictionary(root)
+
+if __name__ == "__main__":
+    main()
